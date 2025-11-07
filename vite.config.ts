a8627a138948e17a453b649c72fa604533d85356
zhanibek <jonimoni84@gmail.com>
2025-11-07 16:42:47 +0500

    Update project configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/demo-landing/", // 👈 добавлено — имя твоего репозитория на GitHub
   server: {
     host: "::",
     port: 8080,
